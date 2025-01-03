9967e55dffcc1ee3e10b93375e65d6bfd88037f3
mabulgu <abulgu@redhat.com>
2025-01-04 00:10:58 +0300

    add note for the marker

--- pkg/machinery/marker.go
+++ pkg/machinery/marker.go
@@ -1,17 +1,8 @@
 /*
-Copyright 2019 The Kubernetes Authors.
+This file is add as a temporary replacement for kubebuilder's marker
+as it doesn't support .rs file extensions currently.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+When contributed to kubebuilder, this file won't be necessary.
 */
 
 package machinery
