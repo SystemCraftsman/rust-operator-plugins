782d990021df32b066022b116ac11af100493e81
mabulgu <abulgu@redhat.com>
2025-02-09 02:48:07 +0300

    replace GOlang with Rust

--- pkg/plugins/rust/domain.go
+++ pkg/plugins/rust/domain.go
@@ -2,5 +2,5 @@ package rust
 
 import plugins "github.com/SystemCraftsman/rust-operator-plugins/pkg"
 
-// DefaultNameQualifier is the suffix appended to all kubebuilder plugin names for Golang operators.
+// DefaultNameQualifier is the suffix appended to all kubebuilder plugin names for Rust operators.
 const DefaultNameQualifier = "rust." + plugins.DefaultNameQualifier
